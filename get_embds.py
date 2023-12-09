5c8abeff7f134055aeb2b85c66345ffbf8276332
Adii0906 <spliots09@gmail.com>
2023-12-09 13:44:10 +0530

    changes made

--- get_embds.py
+++ get_embds.py
@@ -1,7 +1,8 @@
 from langchain.embeddings.base import Embeddings
 from typing import List
 import requests
-
+import aiohttp
+import asyncio
 
 class LocalLlamaEmbeddings(Embeddings):
     def __init__(self, url: str, headers: dict):
@@ -14,36 +15,20 @@ class LocalLlamaEmbeddings(Embeddings):
         response = response.json()
         return response["data"][0]["embedding"]
 
-    def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        # embeddings = []
-
-	    async def post_multiple():
-		    async with aiohttp.ClientSession() as session:
-		    	tasks = []
-		        for text in texts:  # replace with your range
-		            url = self.url  # replace with your API endpoint
-		            payload = {"input": text}  # replace with your payload
-		            async with session.post(url, json=payload) as resp:
-		                data = await resp.json()
-		                tasks.append(data)
-		        responses = await asyncio.gather(*tasks)
-
-		embeddings = asyncio.run(post_multiple())
-
-
-        # for text in texts:
-        #     data = {
-        #         "input": text,
-        #     }
-        #     embeddings.append(
-        #         self.api_query(url=self.url, headers=self.headers, json=data)
-        #     )
-        return embeddings
+    async def post_multiple(self, texts: List[str]) -> List[List[float]]:
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for text in texts:
+                url = self.url  # replace with your API endpoint
+                payload = {"input": text}  # replace with your payload
+                async with session.post(url, json=payload) as resp:
+                    data = await resp.json()
+                    tasks.append(data)
+            embeddings = await asyncio.gather(*tasks)
+            return embeddings
 
     def embed_query(self, text: str) -> List[float]:
         return self.api_query(url=self.url, headers=self.headers, json={"input": text})
-
-
 
 
 async def post_multiple():
@@ -52,8 +37,11 @@ async def post_multiple():
         for number in range(1, 151):  # replace with your range
             url = f'https://your-api-endpoint/{number}'  # replace with your API endpoint
             payload = {"key": "value"}  # replace with your payload
+
             async with session.post(url, json=payload) as resp:
                 data = await resp.json()
                 tasks.append(data)
+
         responses = await asyncio.gather(*tasks)
         # process responses here
+        return responses
